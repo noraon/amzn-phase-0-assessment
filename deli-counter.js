84fa17d5c31103fed3f45737fb6093ad20414ff4
Nora <noraonyi@gmail.com>
2022-04-16 20:43:59 -0400

    completed assignment

--- deli-counter.js
+++ deli-counter.js
@@ -1,24 +1,26 @@
-// 1. Write your functions here
+// Represent queue with an empty array
+
+const katzDeli =[];
+
+function customerLine(array){
+     if(array.length===0) {
+         console.log("The line is currently empty.")
+     }
+}
 
 
-// 2. Example Usage
+function takeNumber(array, name) {
+    let position = array.push(name)
+ console.log(array.push(name))
+ console.log('Welcome, ${name}.You are number 1 in line.')
 
-// const katzDeli = []
 
-// takeANumber(katzDeli, "Ada") //=> Welcome, Ada. You are number 1 in line.
-// takeANumber(katzDeli, "Grace") //=> Welcome, Grace. You are number 2 in line.
-// takeANumber(katzDeli, "Kent") //=> Welcome, Kent. You are number 3 in line.
-
-// line(katzDeli) //=> "The line is currently: 1. Ada 2. Grace 3. Kent"
-
-// nowServing(katzDeli) //=> "Currently serving Ada."
-
-// line(katzDeli) //=> "The line is currently: 1. Grace 2. Kent"
-
-// takeANumber(katzDeli, "Matz") //=> Welcome, Matz. You are number 3 in line.
-
-// line(katzDeli) //=> "The line is currently: 1. Grace 2. Kent 3. Matz"
-
-// nowServing(katzDeli) //=> "Currently serving Grace."
-
-// line(katzDeli) //=> "The line is currently: 1. Kent 2. Matz"
+function nowServing (array) {
+ if (array.length===0){
+        console.log("There is nobody waiting to be served!")
+     } else for (let i =0; i<array.length; i++){
+            array.shift(array[0])
+            console.log(`currently serving ${array[0]}.`)
+     }
+}
+}
